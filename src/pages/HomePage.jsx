da9ef5aba9eb565ec21bcaf398e3e107dcb794f6
Mohammed Raihan <aizvanr@gmail.com>
2026-04-17 08:17:39 +0530

    feat: assemble homepage layout

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import ParticleField from '../components/three/ParticleField'
+import Hero from '../components/Hero'
+import Dashboard from '../components/Dashboard'
+import Footer from '../components/Footer'
+
+export default function HomePage() {
+    return (
+        <main className="relative bg-[#050508] min-h-screen text-white overflow-hidden pb-32">
+            {/* Global immersive 3D background */}
+            <div className="fixed inset-0 z-0">
+                <ParticleField showCubes={true} />
+            </div>
+
+            <Hero />
+            <Dashboard />
+            <Footer />
+        </main>
+    )
+}
